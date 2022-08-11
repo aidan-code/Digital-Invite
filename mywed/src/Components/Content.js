29cea5db9186a035e78e5063a8ab4de805f9701a
Adegbamigbe Opeyemi <fiyinadegbamigbe@gmail.com>
2022-08-11 21:36:52 +0100

    updates

---
+++ mywed/src/Components/Content.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Content.css';
+import Flower from '../Assets/Flower.svg'
+
+const Content = () => {
+  return (
+    <div className="ct-text">
+        <div className="ct-1">
+            <p className="sml">The families of </p>
+
+        <p className="fam-nm">Abayomi-Daniel Adegbamigbe<br/><span className='sep'>&</span><br/>Olawuyi-Olawale </p>
+        <p className="sml"> invites you to the soleminisation of their children </p>
+        </div>
+        
+        <img src={Flower} className="flwr"/>
+
+        <div className='cple'>
+            <span className="col">Folashade </span>
+            <span className="sep">&</span>
+            <span className="col"> Oluwagbemiga</span>
+        </div>
+    </div>
+  )
+}
+
+export default Content
--- mywed/src/Components/Maincp.js
+++ mywed/src/Components/Maincp.js
@@ -1,12 +1,15 @@
 import Nav from './Nav';
 import React from 'react'
 import Banner from './Banner';
+import Content from './Content';
+import './Maincp.css'
 
 const Maincp = () => {
   return (
     <div className='container'>
         <Nav/>
         <Banner />
+        <Content/>
     </div>
   )
 }
