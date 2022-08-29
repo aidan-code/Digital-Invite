6780e8b41f8da2a49d8fe1ba3858674c563eacd8
Adegbamigbe Opeyemi <fiyinadegbamigbe@gmail.com>
2022-08-29 14:25:53 +0100

    Update Rsvp.js

--- mywed/src/Components/Rsvp.js
+++ mywed/src/Components/Rsvp.js
@@ -28,7 +28,7 @@ function handleChange(e){
         <div className="rsvp-form">
 {/* form */}
         <form name="rsvp" 
-        method="POST"
+        action="POST"
         netlify > 
             <label className = "form-sect"> 
                 <p>First Name*</p>
