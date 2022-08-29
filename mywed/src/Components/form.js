618a012099b3a24dc9b2879438e74631ef718505
Adegbamigbe Opeyemi <fiyinadegbamigbe@gmail.com>
2022-08-29 18:54:13 +0100

    Update form.js

--- mywed/src/Components/form.js
+++ mywed/src/Components/form.js
@@ -4,14 +4,16 @@ import './Rsvp.css';
 const Rvp = () => {
   return (
     <div className='ric'>
-        <form method="POST" name="contact" netlify  onSubmit="submit" data-netlify-honeypot="bot-field" >
-
-        <input type="hidden" name="contact" value="contact" />
-            <p className="cryo"><input type="text" name="name" /></p>
-            <p className="cryo"><input type="text" name="lastname"/></p>
-            <p className="cryo"><input type="text" name="Phone"/></p>
-
-            <button type="submit" className="btn">Submit</button>
+        <form name="contact" netlify>
+            <p>
+                <label>Name <input type="text" name="name" /></label>
+            </p>
+            <p>
+                <label>Email <input type="email" name="email" /></label>
+            </p>
+            <p>
+                <button type="submit">Send</button>
+            </p>
         </form>
     </div>
   )
