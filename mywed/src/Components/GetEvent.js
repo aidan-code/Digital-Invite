8a3c1a0e3a0263358a346cc40930c483ad10eb4f
Adegbamigbe Opeyemi <fiyinadegbamigbe@gmail.com>
2022-09-06 09:03:32 +0100

    updates

--- mywed/src/Components/GetEvent.js
+++ mywed/src/Components/GetEvent.js
@@ -11,7 +11,7 @@ function GetEvent() {
        <div className="spacer">
       
       <div class="mapouter">
-        <div class="gmap_canvas"><iframe className='dmap' id="gmap_canvas" title="event" src="https://maps.google.com/maps?q=Whitestone%20Place%20Event%20Centre%203%20Billings%20Way,%20Oregun%20101233,%20Ikeja,%20Lagos%20Nigeria&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+        <div class="gmap_canvas"><iframe className='dmap' id="gmap_canvas" title="event" src="https://maps.google.com/maps?q=Whitestone%20Place%20Event%20Centre%203%20Billings%20Way,%20Oregun%20101233,%20Ikeja,%20Lagos%20Nigeria&t=&z=13&ie=UTF8&iwloc=&output=embed" ></iframe>
       </div>
       </div>
 
@@ -36,3 +36,5 @@ function GetEvent() {
 }
 
 export default GetEvent
+
+// frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
